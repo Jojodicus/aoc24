c34375e52c5cc5d4ed973b3070f855314260f490
Johannes Dittrich <johannes.dittrich@fau.de>
2024-12-11 13:13:37 +0100

    day 11, bad code for now, will fix

--- day11/build.sh
+++ day11/build.sh
@@ -1,1 +1,9 @@
-../common/build.sh
+#! /bin/sh
+
+set -e
+
+mkdir -p .build
+cd .build
+cmake -DOMP=1 ..
+make
+cp aoc ..
--- day11/day11.cpp
+++ day11/day11.cpp
@@ -32,35 +32,89 @@ int main(int argc, char** argv) {
         std::cin.rdbuf(file.rdbuf());
     }
 
-    std::vector<std::pair<long, int>> stones{};
+    std::vector<long> stones{};
     long value;
     while (std::cin >> value) {
-        stones.push_back({value, ITERATIONS});
+        stones.push_back(value);
+    }
+
+    // little bit of caching
+    std::vector<std::vector<long>> cache{};
+    for (int iteration{0}; iteration <= ITERATIONS; ++iteration) {
+        std::vector<long> itVec{};
+        for (int digit{0}; digit < 10; ++digit) {
+            std::vector<std::pair<long, int>> worklist{};
+            worklist.push_back({digit, iteration});
+            long totalIterations{};
+
+            while (!worklist.empty()) {
+                auto [work, it] = worklist.back();
+                worklist.pop_back();
+
+                if (!it) {
+                    // done
+                    totalIterations++;
+                    continue;
+                }
+
+                if (it != iteration && work / 10 == 0) {
+                    // cached
+                    totalIterations += cache[it][work];
+                    continue;
+                }
+
+                // rules
+                if (work == 0) {
+                    worklist.push_back({1, it - 1});
+                } else if (numberOfDigits(work) % 2 == 0) {
+                    auto [left, right] = splitHalf(work);
+                    worklist.push_back({left, it - 1});
+                    worklist.push_back({right, it - 1});
+                } else {
+                    worklist.push_back({work * 2024, it - 1});
+                }
+            }
+
+            itVec.push_back(totalIterations);
+        }
+        cache.push_back(itVec);
     }
 
     // oh boy
     long numberOfStones{};
-    while (!stones.empty()) {
-        auto [work, it] = stones.back();
-        stones.pop_back();
+    #pragma omp parallel for reduction(+ : numberOfStones)
+    for (auto& stone: stones) {
+        std::vector<std::pair<long, int>> worklist{};
+        worklist.push_back({stone, ITERATIONS});
 
-        if (!it) {
-            // done
-            numberOfStones++;
-            continue;
-        }
+        while (!worklist.empty()) {
+            auto [work, it] = worklist.back();
+            worklist.pop_back();
 
-        // rules
-        if (work == 0) {
-            stones.push_back({1, it - 1});
-        } else if (numberOfDigits(work) % 2 == 0) {
-            auto [left, right] = splitHalf(work);
-            stones.push_back({left, it - 1});
-            stones.push_back({right, it - 1});
-        } else {
-            stones.push_back({work * 2024, it - 1});
+            if (!it) {
+                // done
+                numberOfStones++;
+                continue;
+            }
+
+            if (work / 10 == 0) {
+                // cached
+                numberOfStones += cache.at(it).at(work);
+                continue;
+            }
+
+            // rules
+            if (work == 0) {
+                worklist.push_back({1, it - 1});
+            } else if (numberOfDigits(work) % 2 == 0) {
+                auto [left, right] = splitHalf(work);
+                worklist.push_back({left, it - 1});
+                worklist.push_back({right, it - 1});
+            } else {
+                worklist.push_back({work * 2024, it - 1});
+            }
         }
     }
 
-    std::cout << "part 1: " << numberOfStones << std::endl;
+    std::cout << ITERATIONS << " iterations, total: " << numberOfStones << std::endl;
 }
